26da1bb1f7eac260a5bcfe4e95815d66c3bc9011
Adithya R <qat.adithya2305@gmail.com>
2023-04-07 16:01:40 +0530

    Commiting the first version

---
+++ Attendance_2023/Main_File.py
@@ -0,0 +1,24 @@
+from openpyxl import load_workbook
+from all_paths import *
+
+excel = load_workbook(r'C:\Users\User\PycharmProjects\pythonProject1\Attendance_2023\Invoice - March 23.xlsx')
+sheet = excel.active
+
+Mar_Attendance = Attendance()
+Mar_Attendance.login()
+
+for i in range(2, sheet.max_row + 1):
+    Mar_Attendance.add_attendance()
+    Mar_Attendance.month_dropdown()
+    Mar_Attendance.year_dropdown()
+    Mar_Attendance.worked_days(value=sheet.cell(row=i, column=1).value)
+    Mar_Attendance.leaves(value=sheet.cell(row=i, column=2).value)
+    Mar_Attendance.project_dropdown(value=sheet.cell(row=i, column=3).value)
+    Mar_Attendance.employee_id(value=sheet.cell(row=i, column=4).value)
+    Mar_Attendance.verification()
+    sheet.cell(row=i, column=5).value="Done"
+    excel.save(r'C:\Users\User\PycharmProjects\pythonProject1\Attendance_2023\Invoice - March 23.xlsx')
+    print('************************************************************************')
+
+print("Attendance is added for all the employees ")
+
---
+++ Attendance_2023/all_paths.py
@@ -0,0 +1,132 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+
+driver.get("http://175.100.150.82/")
+driver.maximize_window()
+driver.implicitly_wait(5)
+
+
+class Attendance:
+
+    def login(self):
+        driver.find_element("xpath", "//input[@name='name']").send_keys('harsha.ad')
+        driver.find_element("xpath", "//input[@name='pass']").send_keys('12345')
+        driver.find_element("xpath", "//input[@name='op']").click()
+        print("Logged in successfully")
+        time.sleep(2)
+
+    def add_attendance(self):
+        driver.find_element("xpath", "//a[text()='Add attendance']").click()
+        time.sleep(2)
+
+    def worked_days(self, value):
+        # click on Worked days
+        w_days = driver.find_element('xpath', "//input[@id='edit-field-working-days-und-0-value']")
+        w_days.send_keys(value)
+        print(f"Entered Worked days value is {value}")
+
+    def leaves(self, value):
+        # click on Worked days
+        l_days = driver.find_element("xpath", "//input[@id='edit-field-leaves-taken-und-0-value']")
+        l_days.send_keys(value)
+        print(f"Entered leaves days value is {value}")
+
+    def month_dropdown(self):
+        # click on Month dropdown
+        driver.find_element('xpath', "//div[@id='edit_field_month_und_0_value_month_chosen']").click()
+        print("Clicked on Month Dropdown")
+
+        # Enter the Month value in the search box
+        month = driver.find_element("xpath", "//div[@id='edit_field_month_und_0_value_month_chosen']"
+                                             "//div[@class='chosen-drop']//input[@type='text']")
+        month.send_keys("Mar")
+        print(f"Entered value in the Month search field is March")
+        time.sleep(1)
+
+        # click on the suggestion
+        month.send_keys(Keys.ENTER)
+        #driver.find_element("xpath", "//*[@id='edit_field_month_und_0_value_month_chosen']/div/ul/li[1]").click()
+        print("Clicked on the Suggested Month option")
+        time.sleep(2)
+
+    def year_dropdown(self):
+        # click on Year dropdown
+        driver.find_element("xpath", "//div[@id='edit_field_month_und_0_value_year_chosen']").click()
+        print("Clicked on Year Dropdown")
+
+        # Enter the Year value in the search box
+        year = driver.find_element("xpath", "//div[@id='edit_field_month_und_0_value_year_chosen'] "
+                                            "//input[@class='chosen-search-input']")
+        year.send_keys('2023')
+        print("Entered value in the Year search field is 2023")
+        time.sleep(1)
+
+        # click on the suggestion
+        #driver.find_element("xpath", "//*[@id='edit_field_month_und_0_value_year_chosen']/div/ul/li[2]").click()
+        year.send_keys(Keys.ENTER)
+        print("Clicked on the Suggested Year option")
+        time.sleep(2)
+
+    def project_dropdown(self, value):
+        # click on Project dropdown
+        driver.find_element("xpath", "//div[@id='edit_field_project_attend_und_chosen']").click()
+        print("Clicked on Project Dropdown")
+
+        # Enter the Project value in the search box
+        project_dd = driver.find_element("xpath", "//div[@id='edit_field_project_attend_und_chosen'] "
+                                                  "//input[@class='chosen-search-input']")
+        project_dd.send_keys(value)
+        print(f"Entered value in the Project search field is {value}")
+        time.sleep(1)
+
+        # click on the suggestion
+        #driver.find_element("xpath", "//*[@id='edit_field_project_attend_und_chosen']/div/ul/li[1]").click()
+        project_dd.send_keys(Keys.ENTER)
+        print("Clicked on the Suggested project option")
+        time.sleep(2)
+
+    def employee_id(self, value):
+        # click on the authoring information tab
+        driver.find_element("xpath", "//span[normalize-space()='By harsha.ad']").click()
+        print("Clicked on the authoring information tab")
+
+        # clear the existing name
+        emp_id = driver.find_element("xpath", "//input[@id='edit-name']")
+        emp_id.clear()
+        print("Cleared the name")
+
+        # Enter employee id
+        emp_id.send_keys(value)
+        print(f"Entered employee Id is {value}")
+
+        # click on the submit button
+        driver.find_element("xpath", "//*[@id='edit-submit']").click()
+        print("Clicked on the Submit button")
+        time.sleep(2)
+
+    def verification(self):
+        try:
+            ver = driver.find_element("xpath", "//div[@class='messages status']")
+
+            status = ver.text
+            if "has been created" in status:
+                print("Attendance is added successfully")
+
+        except Exception as e:
+            print("Attendance is not added, please re-verify")
+            print(e)
+
+    def get_projectsname(self):
+        self.add_attendance()
+        driver.find_element("xpath", "//div[@id='edit_field_project_attend_und_chosen']").click()
+        projects = (driver.find_elements("xpath", "//li[contains(@class,'active-result')]"))
+
+        pros = []
+        for i in projects:
+            a = i.text
+            pros.append(a)
+
+        print(pros)
